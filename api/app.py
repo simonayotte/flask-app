6fa33bdbf21edec27f0e4415493c589ebd6dc56a
Simon <simonayotte17@gmail.com>
2022-10-13 17:55:06 -0400

    add clustering & instance dynamic response

--- api/app.py
+++ api/app.py
@@ -1,12 +1,28 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+instance_name_url = "http://169.254.169.254/latest/meta-data/tags/instance/Name"
+instance_cluster_type_url = "http://169.254.169.254/latest/meta-data/tags/instance/ClusterType"
 
-@app.route("/")
+
+@app.route("/cluster2")
+def cluster2():
+    hello()
+
+
+@app.route("/cluster1")
+def cluster1():
+    hello()
+
+
 def hello():
-    return "Hello World!"
+    instance_name = requests.get(instance_name_url).text
+    instance_cluster_type = requests.get(instance_cluster_type_url).text
+    return instance_name + " is responding on " + instance_cluster_type
 
 
-if __name__ == "__main":
+if __name__ == "_main_":
     app.run(port=80)
+
