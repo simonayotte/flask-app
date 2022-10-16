e1134be42a32a16a047845896bb7580d91c3695d
Simon <simonayotte17@gmail.com>
2022-10-16 17:10:53 -0400

    add new library for ec2_metadata

--- api/app.py
+++ api/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 import requests
+from ec2_metadata import ec2_metadata
 
 app = Flask(__name__)
-
-instance_name_url = "http://169.254.169.254/latest/meta-data/tags/instance/Name"
-instance_cluster_type_url = "http://169.254.169.254/latest/meta-data/tags/instance/ClusterType"
 
 
 @app.route("/cluster2")
@@ -22,9 +20,9 @@ def cluster():
 
 
 def hello():
-    instance_name = requests.get(instance_name_url).text
-    instance_cluster_type = requests.get(instance_cluster_type_url).text
-    return instance_name + " is responding on " + instance_cluster_type
+    instance_id = ec2_metadata.instance_id
+    instance_type = ec2_metadata.instance_type
+    return instance_id + " is responding on " + instance_type
 
 
 if __name__ == "_main_":
